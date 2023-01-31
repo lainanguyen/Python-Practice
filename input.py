54c804668b4b921adb537c641a007c3ce4678be5
Laina Nguyen <lainanguyen@Lainas-MacBook-Pro.local>
2023-01-30 23:03:14 -0500

    input function

---
+++ input.py
@@ -0,0 +1,7 @@
+# Input function takes input from user and returns it as an output
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+fav_color = input("What's your favorite color? ")
+print(name + " likes " + fav_color)
