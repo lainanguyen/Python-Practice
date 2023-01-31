daeb2a2b966dba54717749714e38a09037ac2c09
lainanguyen <lainanguyen7@gmail.com>
2023-01-31 13:10:49 -0500

    upload arithmetics file

---
+++ arithmetics.py
@@ -0,0 +1,27 @@
+import math
+# Arithmetic Operations
+
+# Basic Functions
+print(10 + 3)
+print(10 -3)
+print(10 * 3)
+print(10/3)
+
+print(10//3)    # Removes decimal value
+print(10 % 3)  # Modulus: Remainder of operation
+print(10 ** 3)  # To the power of
+
+# Increment
+x = 10
+x += 3  # x = x + 3
+print(x)
+
+# Math Functions
+x = 2.9
+print(round(x))  # Rounds up number
+print(abs(-2.9))    # Remove negative
+
+# Math Functions using import math
+print(math.ceil(2.9))   # Rounds up number
+print(math.floor(2.9))  # Rounds down number
+
