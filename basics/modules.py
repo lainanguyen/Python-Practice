1c4da557d65b145856defab7edfb2996f02aa009
lainanguyen <lainanguyen7@gmail.com>
2023-02-03 04:46:02 -0500

    create utility file

---
+++ basics/modules.py
@@ -0,0 +1,9 @@
+import modules_converter
+from modules_converter import kg_to_lbs
+# When we import a specific function from our module, we can easily call that function without prefixing module name
+kg_to_lbs(100)
+
+print(modules_converter.lbs_to_kg(70))
+
+# Challenge
+# Using utils.py
