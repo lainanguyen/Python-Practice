80cd1cbd0b1dc8b0d9963cb9049d0d70a83ebb44
lainanguyen <lainanguyen7@gmail.com>
2023-02-03 05:11:28 -0500

    use random

---
+++ basics/builtin_modules.py
@@ -0,0 +1,21 @@
+import random
+
+# Use built in module to generate random values
+
+for i in range(3):
+    print(random.random())
+    print(random.randint(10, 20))
+
+members = ['John', 'Mary', 'Bob', 'Mosh']
+leader = random.choice(members)
+print(leader)
+
+
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint (1, 6)
+        return first, second
+
+dice = Dice()
+print(dice.roll())
