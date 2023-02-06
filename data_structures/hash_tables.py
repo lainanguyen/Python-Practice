f0494a7c1fed063717eea7fe3ddd7e61a3635c0b
lainanguyen <lainanguyen7@gmail.com>
2023-02-05 21:33:18 -0500

    update hash file

---
+++ data_structures/hash_tables.py
@@ -0,0 +1,36 @@
+# Dictionary
+
+phone_numbers = {
+    'Laina': '1212121212',
+    'Bob': '2323232323',
+    'John': '3434343434'
+}
+
+print(phone_numbers['Laina'])
+
+phone_numbers['Laina'] = '123'
+print(phone_numbers['Laina'])
+
+phone_numbers['New Person'] = '4545454545'
+print(phone_numbers['New Person'])
+
+
+# Hash Tables
+
+class HashTable:
+    def insert(self, key, value):
+        pass
+
+    def find(self, key):
+        pass
+
+    def update(self, key, value):
+        pass
+
+    def list_all(self):
+        pass
+
+
+data_list = [None] * 4096
+print(len(data_list))
+
